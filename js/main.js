70af8f70a678c5a9d6c0754721f4ad0b30dc4e9d
Alexandre Payen <alexandrepayen@yahoo.fr>
2016-05-18 14:25:09 +0200

    Anim mise en place, marche sur Chrome

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function ($) {
+    $(document).ready(function(){
+
+        // hide .navbar first
+        $(".navbar").hide();
+        $('.logo-nav').hide();
+
+        if ($(window).scrollTop()>5){
+            $(window).off('scroll');
+        }
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+
+                // set distance user needs to scroll before we start fadeIn
+                if ($(this).scrollTop() > 5) {
+                    $(this).scrollTop(0);
+                    $('.navbar').fadeIn();
+                    $('.overlay').fadeOut();
+
+                    $('.logo-big').animate({
+                       height :  60
+                    }, 1000);
+
+                    $( "#span-overlay" ).animate({
+                        bottom: 420,
+                        left: -380
+
+
+                    }, 1000, function() {
+                        $('.logo-big').hide();
+                        $('.logo-nav').fadeIn();
+                        $(window).off('scroll');
+                    });
+
+
+
+
+                }
+
+            });
+        });
+
+    });
+}(jQuery));
